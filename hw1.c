a5a66d0f2db4aabbcd5ea182d3c844225a5b96fa
shen-note <90390403+youlinshen@users.noreply.github.com>
2023-10-17 22:41:07 +0800

    Update hw1.c

--- hw1.c
+++ hw1.c
@@ -2,7 +2,7 @@
 
 int main(){
     int i, j;
-    for ( j = 1; j <= 9; j++){
+    for ( j = 1; j <= 9; j+1){
         for ( i = 1; i <= 9; i++)
             printf("%dx%d=%d ", i, j, i*j);
         printf("\n");
