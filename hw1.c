23b2e8b2258b5425499745f4a77192b588ea6f95
shen-note <90390403+youlinshen@users.noreply.github.com>
2023-10-17 22:15:09 +0800

    Create hw1.c

---
+++ hw1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int i, j;
+    for ( j = 1; j <= 9; j++){
+        for ( i = 1; i <= 9; i++)
+            printf("%dx%d=%d ", i, j, i*j);
+        printf("\n");
+    }
+}
