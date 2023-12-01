9350dbd496dc3923f32f3458a12dc8bfde0f4535
shen-note <90390403+youlinshen@users.noreply.github.com>
2023-12-01 11:39:09 +0800

    Update hw1.c

--- lab/hw1.c
+++ lab/hw1.c
@@ -7,14 +7,22 @@ int main(){
     for (i = 0; i < n; i++)
         scanf("%d", &number[i]);
     for (j = n; j > 0; j--){
-    for ( i = 0; i < j-1; i++)
-        if (number[i] > number[i+1]){
-            temp = number[i];
-            number[i] = number[i+1];
-            number[i+1] = temp;
-        }
+        for ( i = 0; i < j-1; i++)
+            if (number[i] > number[i+1]){
+                temp = number[i];
+                number[i] = number[i+1];
+                number[i+1] = temp;
+            }
     }
     for ( i = 0; i < n; i++)
         printf("%d ", number[i]);
+
+    for ( i = 0; i < n; i++)
+        if (number[i] & 1 == 1)
+            printf("%d ", number[i]);
+    printf("\n");
+    for ( i = 0; i < n; i++)
+        if (number[i] & 1 == 0)
+            printf("%d ", number[i]);
     return 0;
 }
