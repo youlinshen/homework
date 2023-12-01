87c579237682e37554f34fd962f949a1d61538af
shen-note <90390403+youlinshen@users.noreply.github.com>
2023-12-01 10:00:09 +0800

    Create hw1.c

---
+++ lab/hw1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+    int n, i, j, k = 0;
+    scanf("%d", &n);
+    int Identity_matrix[n][n];
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            scanf("%d", &Identity_matrix[i][j]);
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            if (Identity_matrix[i][j] != 0 && Identity_matrix[i][j] != 1){
+                printf("否");
+                return 0;
+            }
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++){
+            if (j == k)
+                continue;
+            else if (Identity_matrix[i][j] != 0){
+                printf("否");
+                return 0;
+            }
+        }    
+        if (Identity_matrix[i][k] == 1)
+            k += 1;
+        else{
+            printf("否");
+            return 0;
+        } 
+    printf("是");   
+    return 0;
+}
