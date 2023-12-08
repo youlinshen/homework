b3a653357122c897b65cd2a0b027b3519075ff3f
shen-note <90390403+youlinshen@users.noreply.github.com>
2023-12-08 11:26:40 +0800

    Update hw3.c

--- lab/hw3.c
+++ lab/hw3.c
@@ -9,15 +9,21 @@ int main(){
     float BMI[n];
     for (i = 0; i < n; i++)
         BMI[i] = weight[i]/(height[i]*height[i]);
+    printf("Underweight:");
     for ( i = 0; i < n; i++)
-    {
-        if (BMI[i] = )
-        {
-            /* code */
-        }
-        
-    }
-    
-    
+        if (BMI[i] < 18.5)
+            printf("No.%d ", i+1);
+    printf("Normal:");
+    for ( i = 0; i < n; i++)
+        if (BMI[i] >= 18.5 && BMI[i] < 24)
+            printf("No.%d ", i+1);
+    printf("Overweight:");
+    for ( i = 0; i < n; i++)
+        if (BMI[i] >= 24 && BMI[i] < 27)
+            printf("No.%d ", i+1);
+    printf("Obese:");
+    for ( i = 0; i < n; i++)
+        if (BMI[i] >= 27)
+            printf("No.%d ", i+1);
     return 0;
 }
