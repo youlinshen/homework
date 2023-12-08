607a3bf412f5f19381ae8e60819755601328baf3
shen-note <90390403+youlinshen@users.noreply.github.com>
2023-12-08 11:11:55 +0800

    Create hw3.c

---
+++ lab/hw3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int n, m, i; 
+    scanf("%d", &n);
+    float height[n], weight[n];
+    for (i = 0; i < n; i++)
+        scanf("%f%f", &height[i], &weight[i]);
+    float BMI[n];
+    for (i = 0; i < n; i++)
+    {
+        /* code */
+    }
+    
+    int look_for[m];
+    for ( i = 0; i < m; i++)
+        scanf("%d", &look_for[i]);
+    for ( i = 0; i < m; i++)
+        Look_for(n, look_for[i], class);
+    return 0;
+}
