b0f8740bd513a8887b8489b23811359e903dbdb9
shen-note <90390403+youlinshen@users.noreply.github.com>
2023-12-08 10:31:22 +0800

    Update hw1.c

--- lab/hw1.c
+++ lab/hw1.c
@@ -1,28 +1,18 @@
-#include <stdio.h>
-
 int main(){
-    int n, temp, i, j;
+    int n, m, i, j, sum = 0; 
     scanf("%d", &n);
-    int number[n];
+    int class[n];
     for (i = 0; i < n; i++)
-        scanf("%d", &number[i]);
-    for (j = n; j > 0; j--){
-        for ( i = 0; i < j-1; i++)
-            if (number[i] > number[i+1]){
-                temp = number[i];
-                number[i] = number[i+1];
-                number[i+1] = temp;
-            }
-    }
-    for ( i = 0; i < n; i++)
-        printf("%d ", number[i]);
-
-    for ( i = 0; i < n; i++)
-        if (number[i] & 1 == 1)
-            printf("%d ", number[i]);
-    printf("\n");
-    for ( i = 0; i < n; i++)
-        if (number[i] & 1 == 0)
-            printf("%d ", number[i]);
+        scanf("%d", &class[i]);
+    scanf("%d", &m);
+    int look_for[m];
+    for ( i = 0; i < m; i++)
+        scanf("%d", &look_for[i]);
+    for ( j = 0; j < m; j++)
+        for ( i = 0; i < n; i++){
+            sum += class[i];
+            if (look_for[j] <= sum)
+                printf("%d ", i+1);
+        }
     return 0;
 }
