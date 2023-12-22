685ef88b9626bf629727db3c5db288cfa8d5c0a6
shen-note <90390403+youlinshen@users.noreply.github.com>
2023-12-23 00:10:40 +0800

    1

--- lab/hw4.c
+++ lab/hw4.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 
 int main(){
-    int n, d, i;
+    int n, d, i, p = 0, start = 0;;
     scanf("%d%d", &n, &d);
     int weight[n], max = 0;
     for (i = 0; i < n; i++){
-        scanf("%d", weight[i]);
+        scanf("%d", &weight[i]);
         if (max < weight[i])
             max = weight[i];
     }
-    
-    
+    while (1){
+        int sum = 0;
+        for ( i = start; i < n; i++){
+            if ((sum + weight[i]) <= max)
+                sum += weight[i];
+            else{
+                p += 1;
+                start = i;
+                break;
+            }
+        }
+        if (p+1 > d){
+            max += 1;
+            p = 0;
+            start = 0;
+        }
+        else if (p+1 == d && start == n-1)
+            break;
+    }
+    printf("%d", max);
+    return 0;
 }
