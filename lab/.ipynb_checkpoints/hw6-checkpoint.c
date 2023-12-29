63c9113e8ff7628cfae1c674351762b9856c402d
shen-note <90390403+youlinshen@users.noreply.github.com>
2023-12-29 10:24:44 +0800

    1

--- lab/.ipynb_checkpoints/hw6-checkpoint.c
+++ lab/.ipynb_checkpoints/hw6-checkpoint.c
@@ -6,23 +6,38 @@ typedef struct Node{
     int data;
     struct Node* next;
 } node;
-int insert(node *list1){
-    
+
+int insert(node **ptr, int data){
+    //node *ptr = head;
+    if(*ptr != NULL)
+        for(; *ptr != NULL; *ptr = *ptr->next)
+            if(ptr->next == NULL){
+                ptr->next = (node*)malloc(sizeof(node));
+                ptr = ptr->next;
+                ptr->data = data;
+                ptr->next = NULL;
+                return 0;
+            }
+    else if(ptr == NULL){
+        head = (node*)malloc(sizeof(node));
+        head->data = data;
+        head->next = NULL;
+        return 0;
+    }
 }
 
-int print_list(node *list1){
-    for(; list1 != NULL; list1 = list1->next)
-        printf("%d ", list1->data);
+int print_list(node *head){
+    for(; head != NULL; head = head->next)
+        printf("%d ", head->data);
 }
 
 int main(){
-    node *list1 = (node*)malloc(sizeof(node));
-    assert(list1 != NULL);
-    list1->data = 1;
-    node *list2 = (node*)malloc(sizeof(node));
-    assert(list2 != NULL);
-    list2->data = 2;
-    list1->next = list2;
-    list2->next = NULL;
-    print_list(list1);
+    int data;
+    node *head = NULL;
+    //node *list1 = (node*)malloc(sizeof(node));
+    //assert(list1 != NULL);
+    printf("%d\n", scanf("%d", &data));
+    printf("%p\n", head);
+    insert(&head, data);
+    print_list(head);
 }
