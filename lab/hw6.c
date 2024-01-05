d2947befb63c9a8698a4752c27a53de6a8dd5e08
shen-note <90390403+youlinshen@users.noreply.github.com>
2024-01-05 13:18:50 +0800

    Update hw6.c

--- lab/hw6.c
+++ lab/hw6.c
@@ -6,6 +6,17 @@ typedef struct Node{
     struct Node* next;
 } node;
  
+int freeAll(node *head){
+    node *temp;
+    node *current;
+    current = head;
+    while(current != NULL){
+        temp = current;
+        current = current->next;
+        free(temp);
+    }
+}
+ 
 int insert(node **head, int value){
     node *new = (node*)malloc(sizeof(node));
     new->data = value;
@@ -21,25 +32,35 @@ int insert(node **head, int value){
     return 0;
 }
  
-int delete(node *last){
+int delete(node **head){
+    node *last = *head;
     node *temp = NULL;
-    while(last->next != NULL){
-        temp = last;
-        last = last->next;
-    }
-    free(last);
-    if(temp != NULL)
+    if(last == NULL)
+        return 0;
+    else if(last->next != NULL){
+        while(last->next != NULL){
+            temp = last;
+            last = last->next;
+        }
+        free(last);
         temp->next = NULL;
+    }
+    else{
+        free(last);
+        *head = NULL;
+    }
     return 0;
 }
-
+ 
 int print_list(node *head){
+    if(head == NULL)
+        return 0;
     for(; head != NULL; head = head->next)
         printf("%d ", head->data);
     printf("\n");
     return 0;
 }
-
+ 
 int main(){
     int value;
     int mode;
@@ -51,16 +72,18 @@ int main(){
         }
         switch(mode){
             case 0:
+                freeAll(head);
                 return 0;
             case 1:
                 scanf("%d", &value);
                 insert(&head, value);
                 break;
             case 2:
-                delete(head);
+                delete(&head);
                 break;
             case 3:
                 print_list(head);
+                break;
             default:
                 break;
         }
