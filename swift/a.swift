b11d9ace4346704e58fa2b75b99b1f50fa475630
shen <90390403+youlinshen@users.noreply.github.com>
2025-03-24 02:00:53 +0800

    finish

--- swift/a.swift
+++ swift/a.swift
@@ -18,3 +18,29 @@ var evenNumber = (g % 2 == 0) ? true : false
 if evenNumber {
     print("It is an even number")
 }
+
+var d1 = 1.11 - 1.10 
+var d2 = 2.11 - 2.10
+if abs(d1 - d2) == 0 {
+    print("same")
+} else {
+    print("different")
+}
+
+var t1 = 1.11 - 1.10 
+var t2 = 2.11 - 2.10
+let epsilon = 0.001
+if abs(t1 - t2) <= epsilon {
+    print("Same")
+} else {
+    print("Different")
+}
+
+var value = 9.5
+if value < 0 {
+    print("too small")
+} else if value > 10 {
+    print("too large")
+} else {
+    print("in range")
+}
