47984adce721bb63e9ef80d861422a86c3b22621
youlinshen <90390403+youlinshen@users.noreply.github.com>
2025-03-24 10:51:02 +0800

    Update b.swift

--- swift/b/b.swift
+++ swift/b/b.swift
@@ -1,1 +1,35 @@
+import UIKit
+
+var index = 1...5
+for num in index {
+    print(num*num)
+}
+var sum = 0
+for num in index {
+    sum += num
+}
+print(sum)
+
+
+let numend = 5
+let numstart = 2
+for num in numstart...numend {
+    print(num)
+}
+for num in numstart..<numend {
+    print(num)
+}
+
+
+var a = [1, 2, 4, 5]
+var su = 0
+for num in a {
+    su += num
+}
+print(su)
+
+var deep = ["input", "output", "hidden"]
+for num in deep {
+    print(num)
+}
 
