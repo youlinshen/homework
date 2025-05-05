2ee7b8e3ed76dcd19d8cc1a584a2f4807ccc3323
youlinshen <90390403+youlinshen@users.noreply.github.com>
2025-05-05 10:25:39 +0800

    Update mid.swift

--- swift/mid.swift
+++ swift/mid.swift
@@ -64,3 +64,78 @@ struct RandomDeckOfCards {
         self.cards = allCards.shuffled()
     }
 }
+
+
+
+class GameCards {
+    let deck : RandomDeckOfCards
+    private var top = 0
+    init(){
+        self.deck = RandomDeckOfCards()
+    }
+    func remainingNumOfCards() -> Int{
+        return 52 - top
+    }
+    func getFiveCards() -> [Card] {
+        let getNum = 5
+        var fiveCards = [Card]()
+        if self.remainingNumOfCards() >= getNum{
+            for i in top...top+getNum-1{
+                let card = deck.cards[i]
+                fiveCards.append(card)
+            }
+            top += getNum
+        }
+        return fiveCards
+    }
+}
+
+
+
+struct Hand {
+    let fiveCards : [Card]
+    init(from game: GameCards){
+        self.fiveCards = game.getFiveCards()
+    }
+    func straightFlush()-> Bool {
+        return self.straight() && self.flush()
+    }
+    func flush() -> Bool{
+        var setSuit = Set<Suit>()
+        for card in fiveCards {
+            let suit = card.getSuit()
+            setSuit.insert(suit)
+        }
+        return setSuit.count == 1
+    }
+    func straight() -> Bool{
+        return false
+    }
+    
+    func fourOfaKind() -> Bool{
+        var setRank = Set<Rank>()
+        var set2Rank = Set<Rank>()
+        for card in fiveCards {
+            let rank = card.getRank()
+            if setRank.contains(rank){
+                set2Rank.insert(rank)
+            } else {
+                setRank.insert(rank)
+            }
+        }
+        if setRank.count == 2 && set2Rank.count == 1 {
+            return true
+        }
+        return false
+    }
+    func threeOfaKind() -> Bool{
+        return false
+    }
+    func onePair() -> Bool{
+        return false
+    }
+    func separate() -> Bool{
+        return false
+    }
+
+}
