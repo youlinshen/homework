d5c83fee68ab6e7ceba1c2c1bdb5f3a3039401ae
youlinshen <90390403+youlinshen@users.noreply.github.com>
2025-10-01 00:11:15 +0800

    Add files via upload

---
+++ leetcode20_2.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define MAX_STACK_SIZE 10000
+
+bool isValid(char* s){
+    int len = strlen(s);
+    if((len & 1) == 1)
+        return false;
+
+    char *stack = (char *)malloc(len * sizeof(char));
+    if (stack == NULL)
+        return false;
+    int sp = -1;
+    for(int i = 0;i < len;i++){
+        char a = s[i];
+        if (a == '(' || a == '[' || a == '{'){
+            sp++;
+            stack[sp] = a;
+        }
+        else{
+            if (sp == -1){
+                free(stack);
+                return false;
+            }
+            char top = stack[sp];
+            if((top == '(' && a == ')') ||
+               (top == '[' && a == ']') ||
+               (top == '{' && a == '}'))
+               sp--;
+            else{
+                free(stack);
+                return false;
+            }
+        }
+    }
+    free(stack);
+    return sp == -1;
+};
+
+int main(void){
+
+    char s[MAX_STACK_SIZE] = "([)]";
+    bool a;
+    a = isValid(s);
+    printf("%d",a);
+    return 0;
+}
